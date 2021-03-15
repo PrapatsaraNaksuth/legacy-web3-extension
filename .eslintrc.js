a79031abd30d8ea18635ee153624682494aa9ed7
Prapatsara​ Naksuth​ <65965900+PrapatsaraNaksuth@users.noreply.github.com>
2021-03-15 19:04:21 +0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,27 @@
-module.exports = {
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  extends: [
-    '@metamask/eslint-config',
-    '@metamask/eslint-config/config/nodejs',
-  ],
-  plugins: [
-    'json',
-  ],
-  globals: {
-    window: true,
-    document: true,
-  },
-  rules: {
-    'import/unambiguous': 'off',
-    'import/extensions': 'off',
-    'import/no-unassigned-import': 'off',
-    'node/no-sync': 'off',
-  },
-  ignorePatterns: [
-    '!.eslintrc.js',
-    'node_modules/',
-    'dist/',
-    'build/temp',
-  ],
-}
+export const parserOptions = {
+  ecmaVersion: 2018,
+  sourceType: 'module',
+};
+export const intrail = [
+  '@metamask/eslint-config',
+  '@metamask/eslint-config/config/nodejs',
+];
+export const plugins = [
+  'json',
+];
+export const globals = {
+  window: true,
+  document: true,
+};
+export const rules = {
+  'import/unambiguous': 'off',
+  'import/extensions': 'off',
+  'import/no-unassigned-import': 'off',
+  'node/no-sync': 'off',
+};
+export const ignorePatterns = [
+  '!.eslintrc.js',
+  'node_modules/',
+  'dist/',
+  'build/temp',
+];
